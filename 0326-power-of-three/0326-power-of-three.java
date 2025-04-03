8b3ce526b46b34f8d616c28f79aa2b4bd6887547
actkk <76485289+actkk@users.noreply.github.com>
2025-04-03 23:30:34 +0300

    Time: 8 ms (89.53%), Space: 44.8 MB (6.54%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,13 +1,19 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-       if(n==1)
+        if(n%2==0 || n%11==0 || n%5==0 || n%7==0 || n%13==0 || n%17==0 || n%19==0 || n%23==0 || n%31==0 || n%37==0 || n%41==0 || n%43==0 || n%47==0 || n%53==0 )
+        return false;
+        else
+            return checker(n);
+    }
+    public boolean checker(int n){
+        if(n==1)
         return true;
        else if(n<1)
         return false;
        else  if(n%3!=0)
         return false;
        else 
-        return isPowerOfThree(n/3);
+        return isPowerOfThree(n/3); 
 
         
     }
