32efc78607c4895b83ffb98ccbb9448b3c677e2c
actkk <76485289+actkk@users.noreply.github.com>
2025-04-06 22:43:12 +0300

    Time: 20 ms (23.27%), Space: 55.1 MB (93.6%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+
+
+        int num = nums[0];
+
+        for(int i= 1;i < nums.length ; i++)
+        {
+            if(num==nums[i])
+                return true;
+            num = nums[i];
+
+        }
+        return false;
+        
+    }
+}
