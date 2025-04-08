889c2fa3b2cd379eddbac4a2d4e9fba6e7c6fb6b
actkk <76485289+actkk@users.noreply.github.com>
2025-04-08 14:36:55 +0300

    Time: 0 ms (100%), Space: 41.4 MB (65.55%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,11 +1,8 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        for(int i = 0 ;i <s.length();i++)
-        {
-            String newstrg= s.substring(i)+s.substring(0,i);
-            if(newstrg.equals(goal))
-            return true;
-        }
+        s= s+s;
+        if(goal.length()==s.length()/2 && s.contains(goal))
+         return true;
         return false;
         
     }
