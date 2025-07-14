94e0dc6d0ed337cdce700052e6acced18db5d36d
actkk <76485289+actkk@users.noreply.github.com>
2025-07-14 18:31:20 +0300

    Time: 1 ms (99.88%), Space: 46.5 MB (17.48%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,16 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-    List <Integer> x = new ArrayList<>();
-    for(int i =0 ; i<nums.length; i++)
-    {
-        if (x.contains(nums[i])) 
-            x.remove(Integer.valueOf(nums[i]));
-        else 
-        x.add(nums[i]);
-
-
-    }
-    return x.get(0);
-        
+     int result = 0;
+        for (int num : nums) {
+            result ^= num; 
+        }
+        return result;
     }
 }
